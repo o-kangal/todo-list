017f2e772475d0af6a126773cab4b7e8498b0ded
Onur Kangal <200837@GM.ISBANK.COM.TR>
2024-12-11 16:49:49 +0300

    Setup: Clean up project and add basic structure with SCSS.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './styles.scss';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1>To-Do List Application</h1>
+    </div>
+  );
+};
+
+export default App;
